1cb0577e01f66752dbcda9ab515b4d0100918d7d
Simon Thorpe <simon@hivetechnology.com.au>
2016-02-02 08:58:42 +1100

    remove unused data-auto attr

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ scrollpersist
 =============
 
 Persist page scrolling to last position when user returns back to a visited page.
+No dependencies are required to run.
 
 Simply include:
 ```
@@ -15,7 +16,7 @@ Try the [online demo](https://rawgit.com/simon-thorpe/scrollpersist/master/demo.
 ### Optional Addons
 For smooth animated scrolling you can also include jQuery and jQuery.scrollTo on your page too:
 ```
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/1.4.11/jquery.scrollTo.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.2/jquery.scrollTo.min.js"></script>
 <script src="scrollpersist.js" data-delay="1000"></script>
 ```
--- scrollpersist.js
+++ scrollpersist.js
@@ -4,52 +4,42 @@
  * https://github.com/simon-thorpe/scrollpersist/
  * 
  * Include this script on any page that requires persistant scrolling when the user returns to it:
- * <script src="scrollpersist.js" data-auto="1000"></script>
- * 
- * The data-delay attribute is optional.
- * 
- * Optional plugins:
- * - jQuery and jquery.scrollTo.js for smooth scrolling
+ * <script src="scrollpersist.js" data-delay="1000"></script>
  * 
  */
-(function(){
-	var key='__scrollpersist_'+btoa(window.location.href);
-	var currentScript=document.currentScript;
-	var auto=currentScript.getAttribute('data-auto')!=='no';
-	var delay=null;
-	if(auto){
-		delay=parseInt(currentScript.getAttribute('data-auto'));
-	}
-	var scroll=function(){
-		var oldTop=localStorage[key+'_top'];
-		var oldLeft=localStorage[key+'_left'];
-		if(oldTop||oldLeft){
-			if(typeof(jQuery)!=='undefined'&&typeof(jQuery.scrollTo)!=='undefined'){
-				jQuery.scrollTo({top:oldTop,left:oldLeft},1000);
-			}
-			else{
-				window.scrollTo(oldLeft,oldTop);
-			}
-		}
-	};
-	window.addEventListener('scroll',function(){
-		var doc = document.documentElement;
-		var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
-		var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-		localStorage[key+'_top']=top;
-		localStorage[key+'_left']=left;
-	});
-	if(auto){
-		document.addEventListener('DOMContentLoaded',function(){
-			if(delay){
-				setTimeout(scroll,delay);
-			}
-			else{
-				scroll();
-			}
-		});
-	}
+(function () {
+    var key = '__scrollpersist_' + btoa(window.location.href);
+    var currentScript = document.currentScript;
+    var delay = null;
+    delay = parseInt(currentScript.getAttribute('data-delay'));
+    var scroll = function () {
+        var oldTop = localStorage[key + '_top'];
+        var oldLeft = localStorage[key + '_left'];
+        if (oldTop || oldLeft) {
+            if (typeof (jQuery) !== 'undefined' && typeof (jQuery.scrollTo) !== 'undefined') {
+                jQuery.scrollTo({ top: oldTop, left: oldLeft }, 1000);
+            }
+            else {
+                window.scrollTo(oldLeft, oldTop);
+            }
+        }
+    };
+    window.addEventListener('scroll', function () {
+        var doc = document.documentElement;
+        var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+        var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+        localStorage[key + '_top'] = top;
+        localStorage[key + '_left'] = left;
+    });
+    document.addEventListener('DOMContentLoaded', function () {
+        if (delay) {
+            setTimeout(scroll, delay);
+        }
+        else {
+            scroll();
+        }
+    });
 	
-	// Global function.
-	window.scrollpersist=scroll;
+    // Global function.
+    window.scrollpersist = scroll;
 })();
